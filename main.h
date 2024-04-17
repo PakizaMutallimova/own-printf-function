cf31f5916eb0385730b396b3e3d0694537f19de7
PakizaMutallimova <167027497+PakizaMutallimova@users.noreply.github.com>
2024-04-17 12:10:47 +0400

    Created main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_int(int k, int s);
+int print_str(int k, char *s);
+int print_char(int k, char c);
+
+#endif
